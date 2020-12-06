58cc8f566a31b5403d33d71b5bef9960b425993d
Ana Paula Vale <ana.vale@talkdesk.com>
2020-12-06 18:14:45 +0000

    Add media queries

--- pages/components/Home.jsx
+++ pages/components/Home.jsx
@@ -6,6 +6,14 @@ export default function Home() {
   return (
     <div className={styles.body}>
       <div className={styles.container}>
+        <div className={styles.avatar}>
+          <Image
+            alt="ana-avatar"
+            src="/ana-avatar.jpg"
+            width={243}
+            height={243}
+          />
+        </div>
         <div className={styles.description}>
           <div className={styles.presentation}>
             <h1>Hi, I am Ana, and I’m a Frontend Engineer</h1>
@@ -13,25 +21,22 @@ export default function Home() {
           <div className={styles.social}>
             <ul>
               <li>
-                <FaTwitter size={30} />
+                <a href="https://twitter.com/apvale">
+                  <FaTwitter size={30} />
+                </a>
               </li>
               <li>
-                <FaInstagram size={30} />
+                <a href="https://www.instagram.com/aninhalvale/">
+                  <FaInstagram size={30} />
+                </a>
               </li>
               <li>
-                <FaLinkedin size={30} />
+                <a href="https://www.linkedin.com/in/anapaulavale/">
+                  <FaLinkedin size={30} />
+                </a>
               </li>
             </ul>
           </div>
-        </div>
-        <div className={styles.avatar}>
-          <Image
-            alt="ana-avatar"
-            src="/ana-avatar.jpg"
-            width={243}
-            height={243}
-            quality={100}
-          />
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 import Home from './components/Home'
+import Head from 'next/head'
 
 export default function App() {
-  return <Home />
+  return (
+    <>
+      <Head>
+        <title>Ana's site</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" key="font" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet" key="font-family" />
+      </Head>
+      <Home />
+    </>
+  )
 }
